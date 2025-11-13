1a9f41b6f183e3fe82b7de6968037d8a1bba74f4
linzhanguca <lzhang12@uca.edu>
2025-11-12 19:25:01 -0600

    update motor driver test pins

--- motor_driver.py
+++ motor_driver.py
@@ -26,11 +26,11 @@ class MotorDriver:
 
 # TEST
 if __name__ == "__main__":
-    from time import sleep
+    from utime import sleep
 
     # SETUP
-    # md = MotorDriver(pwm_id=9, in1_id=11, in2_id=10)  # right motor
-    md = MotorDriver(pwm_id=15, in1_id=13, in2_id=14)  # left motor
+    # md = MotorDriver(pwm_id=15, in1_id=13, in2_id=14)  # right motor
+    md = MotorDriver(pwm_id=16, in1_id=18, in2_id=17)  # left motor
     STBY = Pin(12, Pin.OUT)
     STBY.off()
 
